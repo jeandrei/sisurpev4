fa7b2c49088431ec9c48cdca524ce20b9f0b7c6e
jeandrei <jeandreiwalter@gmail.com>
2024-03-15 14:48:23 +0000

    correcao meus certificados

--- sisurpe/app/controllers/Userscertificados.php
+++ sisurpe/app/controllers/Userscertificados.php
@@ -9,16 +9,18 @@
 			}
 
 			public function index($_userId){	
-				$inscricoes = $this->userCertModel->getPresencasByUsuarioId($_userId);
-				foreach($inscricoes as $inscricao){
-					$certificados[] = [
-						'curso' => $this->inscricaoModel->getInscricaoById($inscricao->inscricoes_id),
-						'temas' => $this->temaModel->getTemasInscricoesById($inscricao->inscricoes_id),
-						'usuario' =>$this->userModel->getUserById($_userId),
-						'presencas' =>$this->inscricaoModel->getPresencasUsuarioById($_userId,$inscricao->inscricoes_id)
-					];
-				}
-				
+				if($inscricoes = $this->userCertModel->getPresencasByUsuarioId($_userId)){					
+					foreach($inscricoes as $inscricao){
+						$certificados[] = [
+							'curso' => $this->inscricaoModel->getInscricaoById($inscricao->inscricoes_id),
+							'temas' => $this->temaModel->getTemasInscricoesById($inscricao->inscricoes_id),
+							'usuario' =>$this->userModel->getUserById($_userId),
+							'presencas' =>$this->inscricaoModel->getPresencasUsuarioById($_userId,$inscricao->inscricoes_id)
+						];
+					}
+				} else {
+					$certificados = 'null';
+				}		
 				$data = [
 					'title' => 'Meus Certificados',
 					'certificados' => $certificados
--- sisurpe/app/views/userscertificados/index.php
+++ sisurpe/app/views/userscertificados/index.php
@@ -7,14 +7,19 @@
       <h2><?php echo $data['title'];?></h2>
     </div>
   <hr>    
-  
-  <?php foreach($data['certificados'] as $certificado) : ?>
-    <div class="row border-bottom">
-      <div class="col">
-        <a href="<?php echo URLROOT;?>/inscricoes/certificado/<?php echo $certificado['curso']->id;?>"><?php echo $certificado['curso']->nome_curso; ?></a>        
+  <?php if($data['certificados'] != 'null') : ?>
+    <?php foreach($data['certificados'] as $certificado) : ?>
+      <div class="row border-bottom">
+        <div class="col">
+          <a href="<?php echo URLROOT;?>/inscricoes/certificado/<?php echo $certificado['curso']->id;?>"><?php echo $certificado['curso']->nome_curso; ?></a>        
+        </div>
       </div>
-    </div>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <div class="alert alert-warning" role="alert">
+     Você não possui nenhum certificado!
+    </div>    
+  <?php endif; ?>
  
  
 <?php require APPROOT . '/views/inc/footer.php'; ?>
