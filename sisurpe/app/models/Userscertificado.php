2df39bc2d75d0c29a8745c6fbec168c83850659b
jeandrei <jeandreiwalter@gmail.com>
2024-11-28 10:33:26 +0000

    correcao busca de certificados do usuario

--- sisurpe/app/models/Userscertificado.php
+++ sisurpe/app/models/Userscertificado.php
@@ -6,7 +6,44 @@
 			$this->db = new Database;
 		}
 
-		public function getPresencasByUsuarioId($user_id){
+		public function getPresencasByUsuarioId($user_id){     
+			$this->db->query("	
+        SELECT 
+          i.inscricoes_id  as inscricoes_id ,
+          i.user_id as user_id,
+          i2.nome_curso as nome_curso 
+        FROM 
+          inscritos i ,
+          inscricoes i2 
+          WHERE 
+            EXISTS(
+              SELECT 
+                *
+              FROM 
+                presenca p ,
+                abre_presenca ap 
+              WHERE 
+                p.abre_presenca_id = ap.id 
+              AND 
+                ap.inscricoes_id = i.inscricoes_id 
+              AND 
+                p.user_id = :user_id
+            )
+        AND 
+          i.inscricoes_id = i2.id 
+        AND
+          user_id = :user_id
+			"); 
+			$this->db->bind(':user_id',$user_id);  			
+			$result = $this->db->resultSet(); 
+			if($this->db->rowCount() > 0){					
+				return $result;
+			} else {
+				return false;
+			}           
+    }
+
+    public function getPresencasByUsuarioIdBkp($user_id){
 			$this->db->query("	
         SELECT 
           ap.id, 
@@ -34,6 +71,7 @@
 				return false;
 			}           
     }
+
 
 	}
 ?>
